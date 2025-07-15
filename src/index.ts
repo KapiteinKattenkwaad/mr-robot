52c728da343d0e577b544bdff6c98a75afe264de
KapiteinKattenkwaad <maxstouten@hotmail.com>
2025-07-15 11:30:16 +1000

    changed name of functions

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 
 import readline from 'node:readline';
 
-import { logOutput } from './components/log';
-import { printGrid, moveUp, moveLeft, moveRight, findPlaceCoordinates } from './components/grid';
+import { cliLog } from './components/cliLogger';
+import { printGrid, moveForward, moveLeft, moveRight, findPlaceCoordinates } from './components/robotLogic';
 import { isPlaceInputCorrect } from './util/testPlace';
 
 const rl = readline.createInterface({
@@ -13,7 +13,7 @@ const rl = readline.createInterface({
 
 let isFirstTime = true
 
-logOutput('Welcome to the Robot walk!\n Please choose one of the follow commands:\n PLACE X,Y F\n MOVE\n LEFT or RIGHT\n REPORT \nE.g.: \n PLACE 1,2,EAST');
+cliLog('🤖 Welcome to the Robot walk! \n Please choose one of the follow commands:\n PLACE X,Y F\n MOVE\n LEFT or RIGHT\n REPORT \nE.g.: \n PLACE 1,2,EAST');
 rl.prompt();
 
 
@@ -21,12 +21,12 @@ rl.on('line', (line) => {
   const input = line.trim().toUpperCase();
 
   if (isFirstTime && !isPlaceInputCorrect(input)) {
-    isPlaceInputCorrect(input) ? isFirstTime = false : logOutput('Your first move must be of the command Place. E.g.: PLACE 1,2,EAST')
+    isPlaceInputCorrect(input) ? isFirstTime = false : cliLog('Your first move must be of the command Place. E.g.: PLACE 1,2,EAST')
   } else if (isPlaceInputCorrect(input)) {
     isFirstTime = false
     findPlaceCoordinates(input)
   } else if (input === 'MOVE') {
-    moveUp()
+    moveForward()
   } else if (input === 'LEFT') {
     moveLeft()
   } else if (input === 'RIGHT') {
@@ -36,8 +36,8 @@ rl.on('line', (line) => {
   } else if (input === 'EXIT') {
     rl.close();
   } else {
-    logOutput(`Unknown command: "${input}"`, 'red')
-    logOutput(`E.g.: \n PLACE 1,2,EAST \n MOVE \n LEFT`, 'green')
+    cliLog(`Unknown command: "${input}"`, 'red')
+    cliLog(`E.g.: \n PLACE 1,2,EAST \n MOVE \n LEFT`, 'green')
   }
 
   rl.prompt();
